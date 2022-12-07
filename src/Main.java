f0cc29a18122926d23e459e2883622da7f0f2e3e
lexxxxa82 <lexxxxa.82@mail.ru>
2022-12-07 13:43:26 +0800

    задача из билета 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "строка которую делим";
+
+        final int middle = str.length() / 2;// найдем  середину строки
+        String firstPart = str.substring(0, middle);//применим метод substring
+        String secondPart = str.substring(middle);
+
+        System.out.println(firstPart);
+
+        System.out.println(secondPart);
     }
 }
